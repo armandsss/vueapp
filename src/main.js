46651f379bb8c562ab2600bf765e801a2984be04
Armands Zalitis <azalitis@evolutiongaming.como>
2017-10-02 15:53:55 +0300

    Added vuex store

--- src/main.js
+++ src/main.js
@@ -1,8 +1,9 @@
 import Vue from 'vue'
 import Vuetify from 'vuetify'
-import './stylus/main.styl'
 import App from './App'
+import './stylus/main.styl'
 import router from './router'
+import { store } from './store'
 
 Vue.use(Vuetify)
 Vue.config.productionTip = false
@@ -11,5 +12,6 @@ Vue.config.productionTip = false
 new Vue({
   el: '#app',
   router,
+  store,
   render: h => h(App)
 })
