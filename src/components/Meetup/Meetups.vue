ce7904cf5ed8c3953b4851cdb3c3d2ba07306a10
Armands Zalitis <azalitis@evolutiongaming.como>
2017-10-03 16:16:57 +0300

    changed layout

--- src/components/Meetup/Meetups.vue
+++ src/components/Meetup/Meetups.vue
@@ -2,7 +2,7 @@
   <v-container>
     <v-layout row wrap v-for="meetup in meetups" :key="meetup.id" class="mb-3">
       <v-flex xs12 sm10 md8 offset-sm1 offset-md2>
-        <v-card class="info">
+        <v-card>
           <v-container fluid>
             <v-layout row>
               <v-flex xs5 sm4 md3>
@@ -14,7 +14,7 @@
               <v-flex xs7 sm8 md9>
                 <v-card-title primary-title>
                   <div>
-                    <h5 class="white--text mb-0">{{ meetup.title }}</h5>
+                    <h5 class="black--text mb-0">{{ meetup.title }}</h5>
                     <div>{{ meetup.date | date }}</div>
                   </div>
                 </v-card-title>
